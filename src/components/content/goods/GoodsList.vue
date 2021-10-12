6d2b479a4647753ee0843db74b478d5451a83506
coderkkq <1508035615@qq.com>
2021-10-12 15:46:44 +0800

    传值iid

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="goods-list">
-    <div class="goods-list-item" v-for="(item, index) in goods" :key="index" @click="itemClick">
-      <img :src="item.show.img" alt="">
-      <div class="good-info">
-        <p>{{item.title}}</p>
-        <span class="price">{{item.price}}</span>
-        <span class="collect">{{item.cfav}}</span>
-      </div>
-    </div>
+      <goods-list-item v-for="(item, index) in goods" :goods-item="item" :key="index"/>
   </div>
 </template>
 
 <script>
+import GoodsListItem from './GoodsListItem'
+
 export default {
   name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
   props: {
     goods: {
       type: Array,
@@ -21,11 +19,6 @@ export default {
         return []
       }
     },
-  },
-  methods: {
-    itemClick() {
-      this.$router.push('/detail/' + this.goods.iid)
-    }
   }
 }
 </script>
@@ -37,43 +30,5 @@ export default {
     justify-content: space-around;
     padding: 2px;
   }
-  .goods-list-item {
-    position: relative;
-    width: 48%;
-    padding-bottom: 40px;
-  }
-  .goods-list-item img {
-    width: 100%;
-    border-radius: 5px;
-  }
-  .good-info {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 5px;
-    text-align: center;
-    font-size: 12px;
-  }
-  .good-info p {
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    margin-bottom: 3px;
-  }
-  .good-info .price {
-    margin-right: 20px;
-    color: var(--color-high-text);
-  }
-  .good-info .collect {
-    position: relative;
-  }
-  .good-info .collect::before {
-    position: absolute;
-    top: -1px;
-    left: -15px;
-    content: '';
-    width: 14px;
-    height: 14px;
-    background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
-  }
+ 
 </style>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,51 +1,20 @@
 <template>
   <div id="detail">
-    <!-- <detail-nav-bar class="detail-nav" @titleClick="titleClick" ref="nav"/> -->
-   <!-- <scroll class="content" ref="scroll" @scroll="contentScroll" :probe-type="3">
-     <detail-swiper :top-images="topImages"></detail-swiper>
-     <detail-base-info :goods="goods"/>
-     <detail-shop-info :shop="shop"/>
-     <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
-     <detail-param-info ref="params" :param-info="paramInfo"/>
-     <detail-comment-info ref="comment" :comment-info="commentInfo"/>
-     <goods-list ref="recommend" :goods="recommends"/>
-   </scroll>
-    <detail-bottom-bar @addToCart="addCart"/>
-
-    <back-top @click.native="backClick" v-show="isShowBackTop"/> -->
+    hahah 
   </div>
 </template>
 
 <script>
-// 导入页面子组件
-// import DetailNavBar from './childComponents/DetailNavBar'
-
-// 导入公共子组件
-
-// 导入请求方法
-import {getDetail} from 'network/detail'
 
 export default {
  name: "Detail",
- components: {
-  //  DetailNavBar
- },
+ 
  data() {
    return {
-     iid: null,
-     topImages: [],
-     goods: {}
+    
    }
  },
  created() {
-   this.iid = this.$route.query.iid
-   console.log($route.query.iid);
-
-   getDetail(this.iid).then(res => {
-     const data = res.result;
-     this.topImages = data.itemInfo.topImages;
-     this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
-   })
 
  }
 
@@ -53,18 +22,5 @@ export default {
 </script>
 
 <style scoped>
-   #detail {
-    position: relative;
-    z-index: 9;
-    background-color: #fff;
-    height: calc(100vh - 44px - 49px);
-  }
-  .detail-nav {
-    position: relative;
-    z-index: 9;
-    background-color: #fff;
-  }
-  /* .content {
-    height: 100%;
-  } */
+  
 </style>
